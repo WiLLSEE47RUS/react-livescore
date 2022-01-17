8e6434753302e5468058240b48bf65fc1c2d979f
dbabichev <dbabichev@phoenixit.ru>
2022-01-17 03:02:02 +0300

    LIVESCORE-2 | Реализовать routing

--- src/components/NavLink/NavLink.styled.ts
+++ src/components/NavLink/NavLink.styled.ts
@@ -1,24 +1,25 @@
 import styled from 'styled-components';
-
-export const Wrapper = styled.div`
+import {NavLink} from 'react-router-dom';
+export const LinkWrapper = styled(NavLink)`
   margin-left: 40px;
   height: 80%;
   max-width: 150px;
   padding: 0 15px;
   border-radius: 15px;
-
+  transition: background 0.5s ease;
   :hover {
     background: #849cbc;
     a{
       color: #181818;
     }
   }
-
+ 
   display: flex;
   align-items: center;
   justify-content: center;
+  text-decoration: none;
 `;
-export const Content = styled.a`
+export const Content = styled.span`
   color: var(--navLinkColor);
   font-size: 1.25em;
   text-decoration: none;
